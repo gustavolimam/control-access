459ca7e627d3ee2070f6380c020e96469119aab4
Gustavo Lima <gustavolimash@gmail.com>
2019-11-05 09:43:00 -0300

    created a new page of user base

--- client/src/pages/Users/index.js
+++ client/src/pages/Users/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Typography, Grid, Paper } from "@material-ui/core";
+import { Typography, Grid, Paper, FormControl, TextField, Button } from "@material-ui/core";
 
 import ContentWrapper from "../../components/content-wrapper";
 
@@ -8,8 +8,67 @@ class Users extends Component {
     return (
       <ContentWrapper align="top">
         <Paper style={{ padding: 24 }}>
-          <Grid container xs={12} spacing={24}>
-            <Typography style={styles.textList}>Usuários</Typography>
+          <Grid container xs={12} >
+
+            {/* Formulario */}
+            <Grid item xs={12} sm={9}>
+              <Typography align="center" style={styles.textList}>Cadastrar novos usuários</Typography>
+              <form>
+                {/* Nome e Sobrenome */}
+                <Grid container spacing={10}>
+                  <Grid item xs={12} sm={6}>
+                    {/* Nome */}
+                    <FormControl fullWidth>
+                      <TextField
+                        id="input-nome"
+                        label="Nome"
+                        margin="normal"
+                        variant="outlined"
+                      />
+                    </FormControl>
+                  </Grid>
+                  {/* Sobrenome */}
+                  <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                      <TextField
+                        id="input-sobrenome"
+                        label="RA"
+                        margin="normal"
+                        variant="outlined"
+                      />
+                    </FormControl>
+                  </Grid>
+                </Grid>
+                {/* E-mail e Telefone */}
+                <Grid container>
+                  {/* E-mail */}
+                  <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                      <TextField
+                        id="input-telefone"
+                        label="Telefone"
+                        margin="normal"
+                        variant="outlined"
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                      <TextField
+                        id="input-placa"
+                        label="Placa do veiculo"
+                        margin="normal"
+                        variant="outlined"
+                      />
+                    </FormControl>
+                  </Grid>
+                </Grid>
+              </form>
+              <Button variant="contained" color="primary">
+                Adicionar usuário
+      </Button>
+            </Grid>
+
           </Grid>
         </Paper>
       </ContentWrapper>
@@ -19,7 +78,8 @@ class Users extends Component {
 
 const styles = {
   textList: {
-    fontWeight: "bold"
+    fontWeight: "bold",
+    justifyContent: "center"
   }
 };
 
